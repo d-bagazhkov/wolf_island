b42ff024c33265576da046e94969613d9b4853d7
Dmytro Bagazhkov <36930434+d-bagazhkov@users.noreply.github.com>
2019-08-27 21:39:46 +0300

    start logic move

--- src/main/java/cody/wolf/island/wolfisland/service/StepService.java
+++ src/main/java/cody/wolf/island/wolfisland/service/StepService.java
@@ -9,6 +9,7 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 import java.util.*;
+import java.util.function.Consumer;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 
@@ -29,8 +30,98 @@ public class StepService {
         if (positions.stream().noneMatch(e -> hasText(e.getEntityName()))) {
             return reset();
         }
+        forI(islandConfig.getCountHorizontalCeil(), x -> {
+            forI(islandConfig.getCountVerticalCeil(), y -> {
+                Entity entity = get(x, y, positions);
+                if (!hasText(entity.getEntityName())) return;
+                randomMove(entity, positions);
+            });
+        });
 
         return positions;
+    }
+
+    private void randomMove(Entity entity, List<Entity> positions) {
+        List<Entity> around = new ArrayList<>(around(entity, positions));
+        Entity newPosition;
+        List<Integer> random = IntStream.range(0, around.size()).boxed().collect(Collectors.toList());
+        for (int i = 0; i < around.size(); i++) {
+            newPosition = around.get(random.get(i));
+            if (hasText(newPosition.getEntityName())) continue;
+            get(newPosition.getPosition().getX(), newPosition.getPosition().getY(), positions).setEntityName(entity.getEntityName());
+            entity.setEntityName(null);
+            return;
+        }
+    }
+
+    private Set<Entity> around(Entity entity, List<Entity> positions) {
+        Set<Entity> result = new HashSet<>();
+        result.add(right(entity, positions));
+        result.add(left(entity, positions));
+        result.add(top(entity, positions));
+        result.add(bottom(entity, positions));
+        result.add(leftBottom(entity, positions));
+        result.add(leftTop(entity, positions));
+        result.add(rightBottom(entity, positions));
+        result.add(rightTop(entity, positions));
+        result.remove(null);
+        return result;
+    }
+
+    private Entity right(Entity entity, List<Entity> positions) {
+        if (entity.getPosition().getX() + 1 >= islandConfig.getCountHorizontalCeil())
+            return null;
+        return get(entity.getPosition().getX() + 1, entity.getPosition().getY(), positions);
+    }
+
+    private Entity left(Entity entity, List<Entity> positions) {
+        if (entity.getPosition().getX() - 1 <= 0)
+            return null;
+        return get(entity.getPosition().getX() - 1, entity.getPosition().getY(), positions);
+    }
+
+    private Entity top(Entity entity, List<Entity> positions) {
+        if (entity.getPosition().getY() - 1 <= 0)
+            return null;
+        return get(entity.getPosition().getX(), entity.getPosition().getY() - 1, positions);
+    }
+
+    private Entity bottom(Entity entity, List<Entity> positions) {
+        if (entity.getPosition().getY() + 1 >= islandConfig.getCountVerticalCeil())
+            return null;
+        return get(entity.getPosition().getX(), entity.getPosition().getY() + 1, positions);
+    }
+
+    private Entity leftBottom(Entity entity, List<Entity> positions) {
+        if (entity.getPosition().getY() + 1 >= islandConfig.getCountVerticalCeil()
+                || entity.getPosition().getX() - 1 <= 0)
+            return null;
+        return get(entity.getPosition().getX() - 1, entity.getPosition().getY() + 1, positions);
+    }
+
+    private Entity rightBottom(Entity entity, List<Entity> positions) {
+        if (entity.getPosition().getY() + 1 >= islandConfig.getCountVerticalCeil()
+                || entity.getPosition().getX() + 1 >= islandConfig.getCountHorizontalCeil())
+            return null;
+        return get(entity.getPosition().getX() + 1, entity.getPosition().getY() + 1, positions);
+    }
+
+    private Entity rightTop(Entity entity, List<Entity> positions) {
+        if (entity.getPosition().getY() - 1 <= 0
+                || entity.getPosition().getX() + 1 >= islandConfig.getCountHorizontalCeil())
+            return null;
+        return get(entity.getPosition().getX() + 1, entity.getPosition().getY() - 1, positions);
+    }
+
+    private Entity leftTop(Entity entity, List<Entity> positions) {
+        if (entity.getPosition().getY() - 1 <= 0
+                || entity.getPosition().getX() - 1 <= 0)
+            return null;
+        return get(entity.getPosition().getX() - 1, entity.getPosition().getY() - 1, positions);
+    }
+
+    private void forI(int i, Consumer<Integer> consumer) {
+        IntStream.range(0, i).boxed().forEach(consumer);
     }
 
     private Entity get(Integer x, Integer y, List<Entity> from) {
